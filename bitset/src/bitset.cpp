87c393c46a6ef1764a57bd1806fdc58568e5da11
pyakushev <pavelyakushev77@gmail.com>
2026-04-04 00:16:28 +0300

    Refactor to use raw memory, update .gitignore, update CMakeLists.txt

--- bitset/src/bitset.cpp
+++ bitset/src/bitset.cpp
@@ -2,20 +2,69 @@
 #include <algorithm>
 #include <bit>
 
-bitset::bitset() : bits_(0), true_bits_count_(0) {}
+bitset::bitset() : size_(0), bits_(nullptr), true_bits_count_(0) {}
 
 bitset::bitset(size_t initial_capacity) : 
-    bits_(words_for_bits(initial_capacity), Word(0)),
+    size_(words_for_bits(initial_capacity)),
+    bits_(new Word[size_]()),
     true_bits_count_(0) {}
 
 [[nodiscard]] size_t bitset::size() const noexcept {
-    return bits_.size() * WORD_BITS;
+    return size_ * WORD_BITS;
+}
+
+bitset::bitset(const bitset& other) : 
+    size_(other.size_),
+    bits_(new Word[size_]()),
+    true_bits_count_(other.true_bits_count_)
+{
+    std::copy(other.bits_, other.bits_ + size_, bits_);
+}
+
+bitset::bitset(bitset&& other) noexcept :
+    size_(other.size_), 
+    bits_(other.bits_), 
+    true_bits_count_(other.true_bits_count_) 
+{
+    other.bits_ = nullptr;
+    other.size_ = 0;
+    other.true_bits_count_ = 0;
+}
+
+bitset& bitset::operator=(const bitset& other) {
+    if (this == &other) return *this;
+
+    bitset new_bitset(other);
+    std::swap(size_, new_bitset.size_);
+    std::swap(bits_, new_bitset.bits_);
+    std::swap(true_bits_count_, new_bitset.true_bits_count_);
+    return *this;
+}
+
+bitset& bitset::operator=(bitset&& other) noexcept {
+    if (this == &other) return *this;
+
+    std::swap(bits_, other.bits_);
+    std::swap(size_, other.size_);
+    std::swap(true_bits_count_, other.true_bits_count_);
+    
+    other.bits_ = nullptr;
+    other.size_ = 0;
+    other.true_bits_count_ = 0;
+    
+    return *this;
+}
+
+bitset::~bitset() noexcept {
+    delete[] bits_;
+    size_ = 0;
+    true_bits_count_ = 0;
 }
 
 void bitset::set(size_t k, bool b) {
     const size_t word_idx = k / WORD_BITS;
-    if (word_idx >= bits_.size()) {
-        bits_.resize(word_idx + 1);
+    if (word_idx >= size_) {
+        extend(word_idx + 1);
     }
 
     Word mask = Word(1) << bit_pos_in_word(k);
@@ -30,8 +79,19 @@ void bitset::set(size_t k, bool b) {
     }
 }
 
+void bitset::extend(size_t to_size) {
+    size_t new_size = std::max(2 * size_, to_size);
+    Word* new_bits = new Word[new_size]();
+    std::copy(bits_, bits_ + size_, new_bits);
+    delete[] bits_;
+    bits_ = new_bits;
+    size_ = new_size;
+}
+
 void bitset::clear() {
-    std::fill(bits_.begin(), bits_.end(), Word(0));
+    if (bits_ != nullptr) {
+        std::fill(bits_, bits_ + size_, Word(0));
+    }
     true_bits_count_ = 0;
 }
 
@@ -52,18 +112,18 @@ void bitset::clear() {
 
 [[nodiscard]] bitset bitset::union_with(const bitset& other) const { 
     const auto cmp = [](const bitset& self, const bitset& other) {
-        return self.bits_.size() < other.bits_.size();
+        return self.size_ < other.size_;
     };
     const auto& shorter_bitset = std::min(*this, other, cmp);
     const auto& longer_bitset = std::max(*this, other, cmp);
 
     bitset union_bitset = bitset(longer_bitset.size());
-    for (size_t i = 0; i < shorter_bitset.bits_.size(); ++i) {
+    for (size_t i = 0; i < shorter_bitset.size_; ++i) {
         Word union_word = longer_bitset.bits_[i] | shorter_bitset.bits_[i];
         union_bitset.bits_[i] = union_word;
         union_bitset.true_bits_count_ += std::popcount(union_word);
     }
-    for (size_t i = shorter_bitset.bits_.size(); i < longer_bitset.bits_.size(); ++i) {
+    for (size_t i = shorter_bitset.size_; i < longer_bitset.size_; ++i) {
         union_bitset.bits_[i] = longer_bitset.bits_[i];
         union_bitset.true_bits_count_ += std::popcount(longer_bitset.bits_[i]);
     }
@@ -72,7 +132,7 @@ void bitset::clear() {
 }
 
 [[nodiscard]] bitset bitset::intersection(const bitset& other) const {
-    const auto shorter_size = std::min(bits_.size(), other.bits_.size());
+    const auto shorter_size = std::min(size_, other.size_);
     bitset intersection_bitset = bitset(shorter_size * WORD_BITS);
     for (size_t i = 0; i < shorter_size; ++i) {
         Word intersection_word = bits_[i] & other.bits_[i];
@@ -85,8 +145,8 @@ void bitset::clear() {
 [[nodiscard]] bool bitset::is_subset(const bitset& other) const {
     if (other.true_bits_count_ < true_bits_count_) return false;
 
-    for (size_t i = 0; i < bits_.size(); ++i) {
-        const Word other_word = i < other.bits_.size() ? other.bits_[i] : Word(0);
+    for (size_t i = 0; i < size_; ++i) {
+        const Word other_word = i < other.size_ ? other.bits_[i] : Word(0);
         if (bits_[i] & ~other_word) {
             return false;
         }
--- bitset/src/bitset.h
+++ bitset/src/bitset.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include <cstddef>
 #include <cstdint>
 #include <climits>
@@ -9,23 +8,33 @@ class bitset {
     private:
 
     using Word = uint64_t;
-    static constexpr size_t WORD_BITS = sizeof(Word) * CHAR_BIT; 
-    
+    static constexpr size_t WORD_BITS = sizeof(Word) * CHAR_BIT;
+
     [[nodiscard]] static constexpr size_t words_for_bits(size_t bit_count) noexcept {
         return (bit_count + WORD_BITS - 1) / WORD_BITS;
     }
-    
+
     [[nodiscard]] static constexpr size_t bit_pos_in_word(size_t bit_pos) noexcept {
         return WORD_BITS - 1 - (bit_pos % WORD_BITS);
     }
-    
-    std::vector<Word> bits_;
+
+    size_t size_;
+    Word* bits_;
     size_t true_bits_count_;
-    
+
     public:
 
         bitset();
         explicit bitset(size_t initial_capacity);
+
+        bitset(const bitset& other);
+        bitset(bitset&& other) noexcept;
+
+        bitset& operator=(const bitset& other);
+        bitset& operator=(bitset&& other) noexcept;
+        ~bitset() noexcept;
+
+        void extend(size_t index);
 
         void set(size_t k, bool b);
         [[nodiscard]] bool test(size_t k) const;
@@ -39,3 +48,4 @@ class bitset {
         [[nodiscard]] bool empty() const noexcept;
         void clear();
 };
+
